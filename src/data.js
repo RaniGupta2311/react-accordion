45095094c0b33d79f026ada2554e815d637423de
Rani Gupta <rani.gupta259@gmail.com>
2023-01-25 00:50:47 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+      id: 1,
+      title: 'what is react?',
+      info:
+        'React is an open-source front-end JavaScript library that is used for building user interfaces, especially for single-page applications.',
+    },
+    {
+      id: 2,
+      title: 'what is JSX?',
+      info:
+        'JSX is a XML-like syntax extension to ECMAScript (the acronym stands for JavaScript XML). Basically it just provides syntactic sugar for the React.createElement() function, giving us expressiveness of JavaScript along with HTML like template syntax.',
+    },
+    {
+      id: 3,
+      title: 'What is VirtualDOM?',
+      info:
+        'The Virtual DOM (VDOM) is an in-memory representation of Real DOM. The representation of a UI is kept in memory and synced with the "real" DOM. It is a step that happens between the render function being called and the displaying of elements on the screen.',
+    },
+    {
+      id: 4,
+      title: 'What is state in React?',
+      info:
+        'State of a component is an object that holds some information that may change over the lifetime of the component. We should always try to make our state as simple as possible and minimize the number of stateful components.',
+    },
+    {
+      id: 5,
+      title: 'What is props in React?',
+      info:
+        'Props are inputs to components. They are single values or objects containing a set of values that are passed to components on creation using a naming convention similar to HTML-tag attributes. They are data passed down from a parent component to a child component.',
+    },
+  ]
+  export default questions
